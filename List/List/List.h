79076abd2c87224b47195407887e0366b20dc765
251520921 <251520921@qq.com>
2021-09-24 23:46:13 +0800

    新增了init函数

--- List/List/List.h
+++ List/List/List.h
@@ -8,5 +8,17 @@ private:
 	ListNode<T>* header; ListNode<T>* trailer;
 
 public:
-	
+	void init();
 };
+
+template<class T>
+void List<T>::init() {
+	header = new ListNode<T>;
+	trailer = new ListNode<T>;
+
+	header->succ = trailer;
+	header->pred = NULL;
+	trailer->pred = header;
+	trailer->succ = NULL;
+	_size = 0;
+}
